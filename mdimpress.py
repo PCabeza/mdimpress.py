31a4481ad6c6fd397b730bae38ce616c7b05d6b6
lemniscata <lemniscata.lmn@gmail.com>
2014-12-02 18:29:59 +0100

    template directory

--- mdimpress.py
+++ mdimpress.py
@@ -1,17 +1,20 @@
 #! /usr/bin/python
 
 '''
-Depends of packages:
-- 
+TODO: Add metada blocks
 
-And uses the tool:
-- pandoc
+% title: asdfds
+% author: asfsd
+% description: asddsf
 '''
 
-import argparse, re, codecs, sys, os
+import argparse, re, codecs, sys, os, tempfile
+from os.path import splitext
 from subprocess import Popen,PIPE
 
+MD_HTML_RE = r"\[(?P<text>.*?)(?<!\\)\]<(?P<tag>.*?)>" # uses 'text' and 'tag' groups
 HTML_ELEMENT = r'<%(tag)s %(attr)s>%(body)s</%(tag)s>'
+STYLESHEET = r'<link rel="stylesheet/%(type)s" type="text/css" href="%(href)s"/>'
 
 PANDOC_CALL = ["pandoc", "-t","html5","--section-divs", "-s"]
 TAGATTR_RE = re.compile(r'(.+?)=(.*)')
@@ -38,14 +41,28 @@ def rotation(match):
 
 TRANSLATION_TABLE=(
     (r'(?:^| )([xyz]{1,3})=\(?((?:.*?,){0,2}(?:.*?))\)?(?:$| )', translation,),
-    (r'rot((?:-[xyz]{1,3})?)=\(?((?:.*?,){0,2}(?:.*?))\)?(?:$| )', rotation),
+    (r'(?:^| )rot((?:-[xyz]{1,3})?)=\(?((?:.*?,){0,2}(?:.*?))\)?(?:$| )', rotation),
+    (r'(?:^| )zoom=(.*?)(?:$| )', lambda m: "data-scale=%s" % m.group(1)),
 )
 
-BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),"template")
 TEMPLATE_FILE = os.path.join(BASE_PATH,"impress-template.html")
-IMPRESS_PATH = os.path.join(BASE_PATH, "impress.js")
+
 
 def translation_process(md):
+    '''
+    This function is used to extend pandoc markdown syntax for headings. A heading has the form:
+
+    	# my heading title {#id .class1 attribute1=value1}
+
+    This function uses the regexps from TRANSLATION_TABLE to create custom syntax, for example:
+    
+    	xy=100,-100
+    
+    would be translated to:
+    
+    	data-rotate-x=100 data-rotate-y=-100
+    '''
     table = [(re.compile(i[0]),i[1]) for i in TRANSLATION_TABLE]
     
     for l in md.splitlines():
@@ -63,6 +80,7 @@ def parsetag(gr):
     Parses de body of a tag of the form:
     
     	<tag #id .class1 .class2 attr1=value1 attr2=value2>
+
     And returns a pair (tag, attributes) for use in a html element
     '''
     tag = 'span'
@@ -90,51 +108,88 @@ def elementclass(md):
     To elements of the form `<tag class="class">text</tag>`.
     If `tag` is not included, `span` will be used
     '''
-    finds = re.finditer(r"<(.*?)>\[(.*?)(?<!\\)\]",md)
+    finds = re.finditer(MD_HTML_RE,md)
     for i in list(finds):
-        tag, attr = parsetag(i.group(1))
+        tag, attr = parsetag(i.group('tag'))
         md = md.replace(i.group(0),
                             HTML_ELEMENT % {"tag":tag, "attr":attr,
-                                            "body":i.group(2)})
+                                            "body":i.group('text')})
     return md
 
-
-# parse arguments
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('mdfile', nargs='?')
-parser.add_argument('--output-file','-o', nargs='?')
-parser.add_argument('--extra-css','-css',nargs='?')
-parser.add_argument('--self-contained','-S',action='store_true')
-args = parser.parse_args()
-
-# Choose input file depending on parameters
-if args.mdfile: 
-    with codecs.open(args.mdfile,mode="rd",encoding="utf-8") as f:
-        input_file = f.read()
-else: input_file = sys.stdin.read().decode('utf-8')
-
-if args.self_contained: PANDOC_CALL+=["--self-contained"]
-if args.extra_css: PANDOC_CALL+=["-V","extra-css-url=%s" % (args.extra_css,)]
-
-# Template file is compulsory
-PANDOC_CALL += ["--template", TEMPLATE_FILE]
-# impress js folder
-PANDOC_CALL += ["-V","impress-url=%s" % IMPRESS_PATH ]
+METADATA = {
+    'title': "",
+    'description': type('aux_desc',(object,),{'__repr__':lambda s: METADATA['title']})(),
+    'author': "" 
+}
 
 
+if __name__=="__main__":
+
+    # parse arguments
+    parser = argparse.ArgumentParser(description=''' 
+    		Utility that extends pandoc markdown syntax to easy creation of
+    		impress.js presentations using pandoc utility.''')
+
+    parser.add_argument('mdfile', nargs='?',help='markdown file to compile')
+    parser.add_argument('--output-file','-o', nargs='?',help='save output to this file')
+    parser.add_argument('--stylesheet','-s',action='append', 
+                        help='path to stylesheets file to add')
+    parser.add_argument('--self-contained','-S',action='store_true', 
+                    help='include dependencies in output')
+    parser.add_argument('--external-links','-e',action='store_false', 
+                        dest='self_contained', 
+                        help='force externals to not be included')
+    parser.add_argument('--meta','-m', action='append',
+                        help="update metadata for the presentation, can be any of %s" % 
+    				', '.join(METADATA.keys()))
+    args = parser.parse_args()
+    print args
 
 
-# process input to preparse it
-preprocessed = translation_process(input_file)
-preprocessed = elementclass(preprocessed)
+    # Choose input file depending on parameters
+    if args.mdfile: 
+        with codecs.open(args.mdfile,mode="rd",encoding="utf-8") as f:
+            input_file = f.read()
+    else: input_file = sys.stdin.read().decode('utf-8')
 
 
-# Execute pandoc to generate output
-pid = Popen(PANDOC_CALL, stdin=PIPE,stdout=PIPE)
-out = pid.communicate(preprocessed.encode('utf-8'))
+    METADATA.update(m.split('=',1) for m in args.meta)     # process metadata
 
 
-output_file = sys.stdout
-if args.output_file: output_file = open(args.output_file,mode="w")
+    # Build extra arguments for pandoc call
+    if args.self_contained: PANDOC_CALL+=["--self-contained"]
+    PANDOC_CALL += ["-V","base-url=%s" % BASE_PATH ] # template folder
 
-output_file.write(out[0])
+
+    template_args = {
+        # stylesheet elements
+        'stylesheets': ''.join(
+            [ STYLESHEET % {'type':splitext(s)[1][1:], 'href': s} for s in args.stylesheet])
+    }
+    template_args.update(METADATA)
+
+    temp_template = None    
+    with tempfile.NamedTemporaryFile(mode='wb',delete=False) as template:
+        temp_template = template.name
+        with open(TEMPLATE_FILE, 'r') as orig:
+            template.write(orig.read() % template_args)
+    
+    PANDOC_CALL += ["--template", temp_template] # Template file is compulsory
+
+
+    # process input to preparse it
+    preprocessed = translation_process(input_file)
+    preprocessed = elementclass(preprocessed)
+
+
+    # Execute pandoc to generate output
+    pid = Popen(PANDOC_CALL, stdin=PIPE,stdout=PIPE)
+    out = pid.communicate(preprocessed.encode('utf-8'))
+    
+    # remove temporary template template file 
+    os.unlink(temp_template)
+
+    output_file = sys.stdout
+    if args.output_file: output_file = open(args.output_file,mode="w")
+
+    output_file.write(out[0])
