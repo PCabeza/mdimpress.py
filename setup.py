e6b7108423f118bc2240416e6baf1e7347b5844e
Pablo Cabeza <lemniscata.lmn@gmail.com>
2014-12-18 00:26:35 +0100

    removed comments from setup.py

--- setup.py
+++ setup.py
@@ -9,50 +9,21 @@ import codecs
 from setuptools import setup
 from setuptools.command.install import install
 
-# import glob,os
-
-# GRUNTFILES = filter(os.path.isfile,glob.glob('./grunt/*'))
-# TEMPLATEFILES = filter(os.path.isfile,glob.glob('./template/*'))
-
 setup(
     name='mdimpress.py',
     version="0.1",
     license='GPL',
     author='Pablo Cabeza',
-    author_email='josepablocg@gmail.com',
+    author_email='lemniscata.lmn@gmail.com',
     description='mdimpress.py is a command and framework for building '
                 'presentations from markdown based on impess.js',
-    #long_description=read("README.md"),
-    #url='http://www.geeknote.me',
     packages=['mdimpress'],
     package_data ={'mdimpress': ['data/grunt/*','data/template/*']},
 
-
-    # classifiers=[
-    #     'Development Status :: 3 - Alpha',
-    #     'Intended Audience :: End Users/Desktop',
-    #     'License :: OSI Approved :: GNU General Public License (GPL)',
-    #     'Environment :: Console',
-    #     'Natural Language :: English',
-    #     'Operating System :: OS Independent',
-    #     'Programming Language :: Python',
-    #     'Topic :: Utilities',
-    # ],
-
-    install_requires=[
-        #'distutils'
-    ],
 
     entry_points={
         'console_scripts': [
             'mdimpress = mdimpress:main',
         ]
     },
-#    cmdclass={
-#        'install': full_install
-#    },
-    # platforms='Any',
-    # test_suite='tests',
-    # zip_safe=False,
-    # keywords='Evernote, console'
 )
